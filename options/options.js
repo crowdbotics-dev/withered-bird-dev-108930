b9b0bee5739e6b377b2a07fe1952c2995aeb7e5b
Crowdbotics <engineering@crowdbotics.com>
2023-08-13 23:21:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-bird-dev-108930",
+  url: "https://withered-bird-dev-108930.botics.co",
+  api: "https://withered-bird-dev-108930.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
